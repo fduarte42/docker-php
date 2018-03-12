50e33630b413dc467adcb4fd2eb67d5c7782f492
Tito Duarte <t.duarte@orangehive.de>
2018-03-12 20:32:35 +0100

    [IMPROVEMENT] - removed default VOLUME vom Container

--- build-all.sh
+++ build-all.sh
@@ -5,5 +5,5 @@ VERSIONS="5.3 5.5 5.6 7.0 7.1 7.2"
 
 for V in $VERSIONS; do
     docker build --pull --no-cache -f $V/Dockerfile -t fduarte42/docker-php:$V $V
-    docker build --pull --no-cache -f $V/Dockerfile-debug -t fduarte42/docker-php:$V-debug $V
+    docker build --no-cache -f $V/Dockerfile-debug -t fduarte42/docker-php:$V-debug $V
 done
