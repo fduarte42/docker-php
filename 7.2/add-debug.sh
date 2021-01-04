6c6da48dc42ef7f3bb89decb569ed040be3464d2
Tito Duarte <tito.duarte@interligent.com>
2021-01-04 17:58:58 +0100

    updated 7.2

--- 7.2/add-debug.sh
+++ 7.2/add-debug.sh
@@ -5,10 +5,9 @@ set -e
 pecl channel-update pecl.php.net
 pecl install xdebug
 echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
-echo "xdebug.remote_autostart=on" >> /usr/local/etc/php/conf.d/xdebug.ini
-echo "xdebug.remote_connect_back=off" >> /usr/local/etc/php/conf.d/xdebug.ini
-echo "xdebug.remote_port=9000" >> /usr/local/etc/php/conf.d/xdebug.ini
-echo 'xdebug.remote_host="${XDEBUG_REMOTE_HOST}"' >> /usr/local/etc/php/conf.d/xdebug.ini
-echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini
+echo "xdebug.mode=debug" >> /usr/local/etc/php/conf.d/xdebug.ini
+echo "xdebug.start_with_request=yes" >> /usr/local/etc/php/conf.d/xdebug.ini
+echo "xdebug.client_port=9000" >> /usr/local/etc/php/conf.d/xdebug.ini
+echo 'xdebug.client_host="${XDEBUG_REMOTE_HOST}"' >> /usr/local/etc/php/conf.d/xdebug.ini
 echo "xdebug.idekey=PHPSTORM" >> /usr/local/etc/php/conf.d/xdebug.ini
 echo "display_errors=on" >> /usr/local/etc/php/conf.d/xdebug.ini
