6852b58cab611079bf013c2419ce073db7d5a0ab
Tito Duarte <duartito@gmail.com>
2022-01-17 09:59:00 +0100

    added php symlink to old binary position

--- build/build.sh
+++ build/build.sh
@@ -70,6 +70,7 @@ touch /etc/php/${PHP_VERSION}/mods-available/zzz-custom.ini
 ln -s /etc/php/${PHP_VERSION}/mods-available/zzz-custom.ini /usr/local/etc/php/conf.d/zzz-custom.ini
 ln -s /etc/php/${PHP_VERSION}/mods-available/zzz-custom.ini /etc/php/${PHP_VERSION}/apache2/conf.d
 ln -s /etc/php/${PHP_VERSION}/mods-available/zzz-custom.ini /etc/php/${PHP_VERSION}/cli/conf.d
+ln -s /usr/bin/php /usr/local/bin/php
 ln -s /usr/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
 
 # apcu
