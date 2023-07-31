6f36447cb8b9f72133f596ab00ec0c6bcb1168c9
Tito Duarte <duartito@gmail.com>
2023-07-31 09:49:51 +0100

    added composer 2 for php 7.4

--- build/build.sh
+++ build/build.sh
@@ -186,7 +186,7 @@ curl -o /tmp/composer-setup.sig https://composer.github.io/installer.sig
 php -r "if (hash('SHA384', file_get_contents('/tmp/composer-setup.php')) !== trim(file_get_contents('/tmp/composer-setup.sig'))) { unlink('/tmp/composer-setup.php'); echo 'Invalid installer' . PHP_EOL; exit(1); }"
 
 # Install Composer
-if [[ $PHP_VERSION =~ (7\.2|7\.4) ]]; then
+if [[ $PHP_VERSION =~ (7\.2) ]]; then
   php /tmp/composer-setup.php --1 --no-ansi --install-dir=/usr/local/bin --filename=composer && rm -rf /tmp/composer-setup.php
 else
   php /tmp/composer-setup.php --no-ansi --install-dir=/usr/local/bin --filename=composer && rm -rf /tmp/composer-setup.php
