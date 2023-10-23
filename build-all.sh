ffa45ed562e7faa29fdd4633acf9044efe854bd1
Tito Duarte <tito.duarte@interligent.com>
2023-10-23 16:37:13 +0100

    added oci based sourceguardian image added flavor based images

--- build-all.sh
+++ build-all.sh
@@ -13,29 +13,35 @@ for V in $VERSIONS; do
     docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile -t $BASE_IMAGENAME:$V .
     docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-debug -t $BASE_IMAGENAME:$V-debug .
 
+    # oracle
+    docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-oci -t $BASE_IMAGENAME:$V-oci .
+    docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V --build-arg FLAVOR="-debug" -f Dockerfile-oci -t $BASE_IMAGENAME:$V-oci-debug .
+
     #if [[ $V =~ (7\.2|7\.4) ]]; then
     #    # ioncube
     #    docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-ioncube_loader -t $BASE_IMAGENAME:$V-ioncube_loader .
+    #
+    #    # ioncube with oracle
+    #    docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V --build-arg FLAVOR="-oci" -f Dockerfile-ioncube_loader -t $BASE_IMAGENAME:$V-oci-ioncube_loader .
     #fi
 
     if [[ $V =~ (7\.2|7\.4|8\.0|8\.1) ]]; then
         # sourceguardian
         docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-sourceguardian -t $BASE_IMAGENAME:$V-sourceguardian .
+
+        # sourceguardian with oracle
+        docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V --build-arg FLAVOR="-oci" -f Dockerfile-sourceguardian -t $BASE_IMAGENAME:$V-oci-sourceguardian .
     fi
 
     if [[ $V =~ (7\.2|7\.4|8\.0|8\.1) ]]; then
         # chartdirector
         docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-chartdirector -t $BASE_IMAGENAME:$V-chartdirector .
-        docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-chartdirector-debug -t $BASE_IMAGENAME:$V-chartdirector-debug .
+        docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V --build-arg FLAVOR="-debug" -f Dockerfile-chartdirector -t $BASE_IMAGENAME:$V-chartdirector-debug .
     fi
-
-    # oracle
-    docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-oci -t $BASE_IMAGENAME:$V-oci .
-    docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-oci-debug -t $BASE_IMAGENAME:$V-oci-debug .
 
     # sqlsrv
     #docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-sqlsrv -t $BASE_IMAGENAME:$V-sqlsrv .
-    #docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V -f Dockerfile-sqlsrv-debug -t $BASE_IMAGENAME:$V-sqlsrv-debug .
+    #docker buildx build --platform $PLATFORMS --push --pull --no-cache --build-arg BASE_IMAGENAME=$BASE_IMAGENAME --build-arg PHP_VERSION=$V --build-arg FLAVOR="-debug" -f Dockerfile-sqlsrv -t $BASE_IMAGENAME:$V-sqlsrv-debug .
 done
 
 cd ..
