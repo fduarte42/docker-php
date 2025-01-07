d35b6d496d7a7e91c6e39a55c1e814c3e9fa9b5a
Tito Duarte <tito.duarte@interligent.com>
2025-01-07 17:45:06 +0000

    remove python and add none interactive mode

--- build/build.sh
+++ build/build.sh
@@ -19,6 +19,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update
+apt-get remove -y --purge python3 python3-minimal python3.*
 apt-get upgrade -y
 
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula \
