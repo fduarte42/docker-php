8104fb655b906ab9d33bd055d0cf604dfaa6b89d
Tito Duarte <duartito@gmail.com>
2025-02-15 21:19:11 +0000

    updated node to 22

--- build/build-slim.sh
+++ build/build-slim.sh
@@ -16,7 +16,7 @@ sed -i 's/^Components: main$/& contrib/' /etc/apt/sources.list.d/debian.sources
 # node
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update
 apt-get remove -y --purge python3 python3-minimal python3.*
